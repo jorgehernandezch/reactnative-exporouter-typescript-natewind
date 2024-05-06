e4ee40e5cc168401e425185488d64ccd95b59914
Jorge Edo. Hernández <jorge@galpha.co>
2024-05-05 23:04:17 -0300

    Setup project

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TabsIndex() {
+  return (
+    <View className="bg-cyan-300 flex-1 justify-center items-center">
+      <Text>Oka</Text>
+    </View>
+  )
+}
---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Slot } from 'expo-router'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import '@/styles/global.css'
+
+export default function Layout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slot />
+    </GestureHandlerRootView>
+  )
+}
