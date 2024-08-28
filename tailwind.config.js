5d04e2e8c2dd503807e33836855b4551e2c388f3
Jorge Edo. Hernández <jorge@galpha.co>
2024-08-28 12:32:02 -0300

    Mov fonts and colors to Constants Folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import { colors } from './src/styles/colors'
-import { fontFamily } from './src/styles/fonts'
+import { colors } from './src/constants/colors'
+import { fontFamily } from './src/constants/fonts'
 
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
